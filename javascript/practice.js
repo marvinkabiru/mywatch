072dd461061be2429584a6e214c7251fec23876e
marvinkabiru <marvinkabiru@gmail.com>
2019-10-08 05:59:35 +0300

    firstpush

---
+++ javascript/practice.js
@@ -0,0 +1,35 @@
+// var cars = ["Volvo", "Toyota", "BMW", "Mercedes", "Audi"];
+// //condition1
+// if(car == "BMW"){
+//     document.write("BMW Match");
+// }else{
+//     document.write("Not BMW Match");
+// }
+// //condition2
+// if(car == "BMW" || car == "Audi"){
+//     document.write("Pure German Match");
+// }else{
+//     document.write("Not Pure German Match");
+// }
+//calculating the length an array using code
+function myFunction() {
+var cars = ["Volvo", "Toyota", "BMW", "Mercedes", "Audi"];
+document.getElementById("practice").innerHTML = cars. length;
+<button onclick="myFunction()">check</button>
+}
+
+
+
+
+
+{/* <p>Click the button to create an array, then display its length.</p>
+
+<button onclick="myFunction()">Try it</button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+  var fruits = ["Banana", "Orange", "Apple", "Mango"];
+  document.getElementById("demo").innerHTML = fruits.length;
+} */}
